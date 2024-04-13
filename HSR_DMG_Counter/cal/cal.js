7dc8f94c89d3d0821b60b3c776a83e26ee6c7929
JHao <109132177+lemonT13@users.noreply.github.com>
2024-04-13 15:35:25 +0800

    Add files via upload

---
+++ HSR_DMG_Counter/cal/cal.js
@@ -0,0 +1,31 @@
+// 获取URL参数
+function getQueryParam(paramName) {
+    var params = new URLSearchParams(window.location.search);
+    return params.get(paramName);
+}
+
+// 根据角色ID加载相应的CSS和JS文件
+function loadCharacterFiles(characterId) {
+    var cssFile = `charactor/${characterId}.css`;
+    var jsFile = `charactor/${characterId}.js`;
+
+    // 动态加载CSS文件
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = cssFile;
+    document.head.appendChild(link);
+
+    // 动态加载JS文件
+    var script = document.createElement('script');
+    script.src = jsFile;
+    document.head.appendChild(script);
+}
+
+// 在页面加载时调用loadCharacterFiles函数
+document.addEventListener('DOMContentLoaded', function() {
+    var characterId = getQueryParam('characterId');
+    if (characterId) {
+        loadCharacterFiles(characterId);
+
+    }
+});
